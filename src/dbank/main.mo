1ebb75247bad5749704f6627de8cd919b7346a12
Yashpriyadeep katta <yashkatta2000@gmail.com>
2022-09-26 12:39:51 +0530

    Complete All

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,44 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentValue: Float = 100;
+  currentValue := 300;
+  Debug.print(debug_show(currentValue));
+
+  stable var startTime = Time.now();
+  startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  let id = 234873120874; //we can not change value of id, since it is not var but id
+  // Debug.print(debug_show(id));
+
+  public func topUp(amount: Float){
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  public func withDraw(amount: Float){
+    let tempValue: Float = currentValue - amount; // After the colon the data type is defined,
+    if(tempValue >= 0){                         //  we could have also done it in case of
+      currentValue -= amount;                   // currentValue but since we are assigning it a value
+      Debug.print(debug_show(currentValue));    // itself so it is not necessary!
+    }
+    else 
+      Debug.print("Insufficient balance")
+    
+  };
+
+  public query func checkBalance(): async Float{
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS  = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.001 ** Float.fromInt(timeElapsedS));
+    startTime := currentTime;
+  }
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,52 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function(){ 
+  //We placed async here because we
+  // because we are calling checkBalance func and if we see the func defination of 
+  //checkBalance, we will find out that the currentValue is returned asynchrously
+  //i.e we are not actually waiting for the result to comeback, and instead we recievce a 
+  //promise object which is returned at any point which depends on how long it takes
+  //to query the blockchain.
+  //hence, we also place async in before the call-back function in the current function i.e
+  //to adapt.
+  // console.log("Finishing loading");
+ update();
+});
 
-  const name = document.getElementById("name").value.toString();
+document.querySelector("form").addEventListener("submit", async function(event) {
+  //you can wee that after enetering number and submitting, you see submitted really fast
+  // this is because form has a default action of form clear ans submit value
+  // hence we add prevent default
+  event.preventDefault();
+  // console.log("submitted!"); 
+  const button = event.target.querySelector("#submit-btn");
+  
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawl-amount").value);
 
   button.setAttribute("disabled", true);
+  if(document.getElementById("input-amount").value.length != 0){
+    await dbank.topUp(inputAmount);
+  }
+  if(document.getElementById("withdrawl-amount").value.length != 0){
+    await dbank.withDraw(outputAmount);
+  }
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
+  await dbank.compound();
 
+  update();
+  document.getElementById("input-amount").value="";
+  document.getElementById("withdrawl-amount").value="";
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+  //The line 24, 29 and 38 are used so that when the user click the submit button, the button can get
+  //disabled till the result is fetched, as we know that it takes time to get result through update method
+  //hence we disable the button so that user doesn't apply say multiple topUp opersation
 });
+
+async function update(){
+  const currentAmount = await dbank.checkBalance();
+  // we are placing await keyword, so that we can wait for result to comeback and that asynchronously
+  //update are element with the currentAmount once it does return
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+
+}
